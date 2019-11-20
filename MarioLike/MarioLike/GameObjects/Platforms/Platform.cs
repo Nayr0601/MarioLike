054a756416ce43e86f3e6d1ac40c39f0c1bb921f
gamergod69 <gamergod69@users.noreply.github.com>
2019-11-20 10:12:51 +0100

    MarioLike V0.13

---
+++ MarioLike/MarioLike/GameObjects/Platforms/Platform.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarioLike.GameObjects.Platforms
+{
+    class Platform : GameObject
+    {
+        public override void Update(GameTime gameTime)
+        {
+
+        }
+    }
+}
